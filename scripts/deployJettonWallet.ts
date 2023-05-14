078dfdaacdf264a9832ba9615f9580368a92aa99
Gusarich <danielsedovzzz@gmail.com>
2023-05-14 13:45:58 +0300

    jettonWallet deployment script

--- scripts/deployJettonWallet.ts
+++ scripts/deployJettonWallet.ts
@@ -1,13 +1,17 @@
-import { toNano } from 'ton-core';
+import { Address, toNano } from 'ton-core';
 import { JettonWallet } from '../wrappers/JettonWallet';
-import { compile, NetworkProvider } from '@ton-community/blueprint';
+import { NetworkProvider } from '@ton-community/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const jettonWallet = provider.open(JettonWallet.createFromConfig({}, await compile('JettonWallet')));
+    const jettonWallet = provider.open(
+        JettonWallet.createFromConfig({
+            owner: Address.parse('EQBIhPuWmjT7fP-VomuTWseE8JNWv2q7QYfsVQ1IZwnMk8wL'),
+            balance: toNano('1'),
+            master: Address.parse('EQB8GJpiN7YxxKak6O2wH-aAEVaVrzJKuq9qYK6WHGUSHEjv'),
+        })
+    );
 
-    await jettonWallet.sendDeploy(provider.sender(), toNano('0.05'));
+    await jettonWallet.sendDeploy(provider.sender(), toNano('0.0001'));
 
     await provider.waitForDeploy(jettonWallet.address);
-
-    // run methods on `jettonWallet`
 }
